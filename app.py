cbb4a27c675f715f65b7f38d13828f1095f1cef3
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-23 09:46:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-
-# example
-
 import streamlit as st
-import numpy as np
 
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
+# display user message
+with st.chat_message("user"):
+	st.write("Hello 👋")
+
+# display assistant message
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
+
+# user input
+with st.chat_input("Say something") as prompt:
+	if prompt:
+		st.write(f"User has sent the following prompt: {prompt}")
