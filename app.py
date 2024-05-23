da66941e350f2c4d75608a82bc6022f7e13ba4a3
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-23 09:56:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,26 @@ st.title("Echo Bot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+	st.session_state.messages = []
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
 	with st.chat_message(message["role"]):
-    	st.markdown(message["content"])
+		st.markdown(message["content"])
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+	# Display user message in chat message container
+	with st.chat_message("user"):
+		st.markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+	st.session_state.messages.append({"role": "user", "content": prompt})
 
 	# mirror the user input
 	response = f"Echo: {prompt}" # define the response function as you want
 	# Display assistant response in chat message container
 	with st.chat_message("assistant"):
-	    st.markdown(response)
+		st.markdown(response)
 	# Add assistant response to chat history
 	st.session_state.messages.append({"role": "assistant", "content": response})
 
