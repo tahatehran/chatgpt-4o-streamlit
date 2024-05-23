5ee41738b5cadbed490f10f45eb3c0622070e526
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-23 13:27:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def check_supabase_file_exists(file_path):
 
 def upload_file_to_supabase_storage(file_obj):
 	base_name = os.path.basename(file_obj.name)
-	path_on_supastorage = os.path.splitext(base_name)[0] + '_' + str(round(time.time()))  + os.path.splitext(base_name)[1]
+	path_on_supastorage = os.path.splitext(base_name)[0] + '_' + str(round(time.time())//600)  + os.path.splitext(base_name)[1]
 	mime_type, _ = mimetypes.guess_type(file_obj.name)
 	
 	supabase = get_supabase_client()
