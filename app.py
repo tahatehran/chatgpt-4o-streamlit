4b1eb478fe29799dfc53c8308238fa9d490d9271
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-23 09:50:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ with st.chat_message("assistant"):
     st.bar_chart(np.random.randn(30, 3))
 
 # user input
-with st.chat_input("Say something") as prompt:
-	if prompt:
-		st.write(f"User has sent the following prompt: {prompt}")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
