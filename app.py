c6de01e96187ca3c9247dee382bcbf7b6aca3ab3
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-23 11:39:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,36 @@ import streamlit as st
 import numpy as np
 import random
 import time
+from PIL import Image
+
+# check if image
+def is_image(file_path):
+    try:
+        Image.open(file_path)
+        return True
+    except IOError:
+        return False
+
+from supabase import create_client, Client
+def get_supabase_client():
+    url = st.secrets['supabase_url']
+    key = st.secrets['supabase_key']
+    supabase = create_client(url, key)
+    return supabase
+
+def upload_file_to_supabase_storage(file_path):
+    path_on_supastorage = os.path.basename(file_path)
+    mime_type, _ = mimetypes.guess_type(file_path)
+
+    supabase = get_supabase_client()
+    bucket_name = st.secrets["bucket_name"]
+    with open(file_path, 'rb') as f:
+        supabase.storage.from_(bucket_name).upload(file=f,path=path_on_supastorage, file_options={"content-type": mime_type})
+    
+    public_url = supabase.storage.from_(bucket_name).get_public_url(path_on_supastorage)
+    return public_url
+
+
 
 st.title("Echo Bot")
 
@@ -35,9 +65,11 @@ with st.sidebar:
 	if uploaded_file is not None:
 		# display filename
 		# st.write("Filename:", uploaded_file.name)
+		public_url = upload_file_to_supabase_storage(uploaded_file.name)
+		print(public_url)
 		if uploaded_file.type.startswith("image/"):
 			st.image(uploaded_file)
-					   
+
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
