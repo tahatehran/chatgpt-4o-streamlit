865c0fd14ff36cd1d8fd063742a8a56cb6c4ed77
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-23 09:55:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 import streamlit as st
 import numpy as np
 
-# display user message
-with st.chat_message("user"):
-	st.write("Hello 👋")
+st.title("Echo Bot")
 
-# display assistant message
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# user input
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+	with st.chat_message(message["role"]):
+    	st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+	# mirror the user input
+	response = f"Echo: {prompt}" # define the response function as you want
+	# Display assistant response in chat message container
+	with st.chat_message("assistant"):
+	    st.markdown(response)
+	# Add assistant response to chat history
+	st.session_state.messages.append({"role": "assistant", "content": response})
+
