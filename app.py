8f2ca19cd602344513cd7ea430d11452fdcaeab9
zluckyH <zluckyhou@users.noreply.github.com>
2024-05-24 12:55:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ with st.sidebar:
 
 	st.divider()
 
-	st.markdown("Buy me a coffee with Paypal")
+	st.markdown("**Buy me a coffee with Paypal**")
 	st.image("https://wbucijybungpjrszikln.supabase.co/storage/v1/object/public/chatgpt-4o/Buy%20Me%20a%20Coffe-qrcode_2860875.png")
 
 	st.divider()
